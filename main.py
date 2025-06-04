c0ada9447d462ba96eab0ae8f744c7aa9240be69
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-06-04 15:45:47 -0300

    atualização

---
+++ main.py
@@ -0,0 +1,77 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.5
+
+# Passo 1: Entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# Abrir o Chrome
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+time.sleep(3)
+
+# Digitar o site
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+
+time.sleep(3)
+
+# Passo 2: Fazer login
+# Preencher e-mail
+pyautogui.click(x=523, y=370)
+pyautogui.write("lucas@gmail.com")
+
+# Preencher senha
+pyautogui.press("tab")
+pyautogui.write("7g7gyxb6mp5fu")
+
+# Botão logar
+pyautogui.press("tab")
+pyautogui.press("enter")
+
+time.sleep(3)
+
+# Passo 3: Importar a base de dados
+import pandas
+
+tabela = pandas.read_csv("produtos.csv")
+
+# Passo 4: Cadastrar 1 produto
+for linha in tabela.index:
+    pyautogui.click(x=508, y=263)
+
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(codigo)
+
+    pyautogui.press("tab")
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(marca)
+
+    pyautogui.press("tab")
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+
+    pyautogui.press("tab")
+    categoria = str(tabela.loc[linha, "categoria"])
+    pyautogui.write(categoria)
+
+    pyautogui.press("tab")
+    preco_unitario = str(tabela.loc[linha, "preco_unitario"])
+    pyautogui.write(preco_unitario)
+
+    pyautogui.press("tab")
+    custo = str(tabela.loc[linha, "custo"])
+    pyautogui.write(custo)
+
+    pyautogui.press("tab")
+    obs = str(tabela.loc[linha, "obs"])
+    if obs != "nan":
+        pyautogui.write(obs)
+
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+    pyautogui.scroll(10000)
+
+# Passo 5: Repetir para todos os produtos
